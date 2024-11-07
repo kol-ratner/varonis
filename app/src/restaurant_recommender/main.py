8365c17eaa0dd2625bc333c4d8dad1739a309eb2
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-07 18:56:56 +0200

    clean up

--- app/src/restaurant_recommender/main.py
+++ app/src/restaurant_recommender/main.py
@@ -20,7 +20,6 @@ logger = logging.getLogger(__name__)
 db_config = DatabaseConfig(
     # Get connection details from environment variables with defaults
     user=os.getenv("DB_USER", "postgres"),
-    # password = os.getenv("DB_PASSWORD", "postgres"),
     database=os.getenv("DB_NAME", "restaurant_db"),
     host=os.getenv("DB_HOST", "localhost"),
     port=os.getenv("DB_PORT", "5432")
