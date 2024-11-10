f575e04681e889f2f9ff4141ad38800015059aea
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-10 20:49:30 +0200

    fix k8s

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "k3s" {
   name                = "k3s-ip"
-  location            = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
 
