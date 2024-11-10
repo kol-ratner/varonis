5631c0a69233fc67f9f0dac140649236675fe658
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-10 20:54:04 +0200

    extending nsg for pub subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -28,6 +28,28 @@ resource "azurerm_network_security_group" "pub_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-kubernetes-api"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "6443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "pub" {
