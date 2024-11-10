3d21eac8a95329818c0f354006ccab7655285d95
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-10 21:13:32 +0200

    update k8s vm

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -18,16 +18,27 @@ resource "azurerm_network_interface" "k3s" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "k3s" {
+  network_interface_id      = azurerm_network_interface.k3s.id
+  network_security_group_id = azurerm_network_security_group.priv_nsg.id
+}
+
+resource "azurerm_storage_account" "k3s" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = data.azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_linux_virtual_machine" "k3s" {
   name                = "k3s"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
-  admin_username      = "adminuser"
-
   network_interface_ids = [
     azurerm_network_interface.k3s.id
   ]
+  size = "Standard_B2s"
 
   os_disk {
     caching              = "ReadWrite"
@@ -36,9 +47,16 @@ resource "azurerm_linux_virtual_machine" "k3s" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
+  }
+
+  computer_name  = "hostname"
+  admin_username = "admin"
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.k3s.primary_blob_endpoint
   }
 
   custom_data = base64encode(<<-EOF
