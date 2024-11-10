45d5b2c6b08b56ca9cf41ee023448ca3df3b45de
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-10 22:52:38 +0200

    mute

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-# Add output to get the public IP
-output "public_ip" {
-  value = azurerm_public_ip.k3s.ip_address
-}
+# # Add output to get the public IP
+# output "public_ip" {
+#   value = azurerm_public_ip.k3s.ip_address
+# }
