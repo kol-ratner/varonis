611b774d777edbc0b2548144f03d89b4622565bd
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-11 08:12:30 +0200

    cleaner code organization in the pre-tf subdir

--- pre-tf/github-actions.tf
+++ pre-tf/github-actions.tf
@@ -1,21 +1,6 @@
 data "azurerm_subscription" "current" {}
 data "azuread_client_config" "current" {}
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstaterestaurantrec"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
-
-
 resource "azuread_application" "github_actions" {
   display_name = "github-actions-terraform"
   owners       = [data.azuread_client_config.current.object_id]
