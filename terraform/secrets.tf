c75bd00fdbc8766831b0f134497e9ee0c4b08be1
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-13 08:59:14 +0200

    update

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "kv" {
-  name                = "restaurant-recommender-kv"
+  name                = "restaurants-kv"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
