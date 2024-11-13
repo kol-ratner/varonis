448b762ea6fde3e6e055c6423b61b92c5c3380e7
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-13 08:54:28 +0200

    update

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,7 +1,3 @@
-resource "random_password" "db_admin" {
-  length  = 20
-  special = true
-}
 
 resource "azurerm_postgresql_flexible_server" "db" {
   name                = "restaurant-db"
@@ -12,12 +8,18 @@ resource "azurerm_postgresql_flexible_server" "db" {
   storage_mb = 32768
 
   administrator_login    = "psqladmin"
-  administrator_password = random_password.db_admin.result
+  administrator_password = azurerm_key_vault_secret.db_admin_password.value
 
   zone = "1"
 
   #   private_dns_zone_id = azurerm_private_dns_zone.priv.id
   delegated_subnet_id = azurerm_subnet.priv_subnet.id
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "restaurants_user" {
+  name      = "restaurants_user"
+  server_id = azurerm_postgresql_flexible_server.db.id
+  value     = "CREATE USER restaurants_user WITH PASSWORD '${azurerm_key_vault_secret.restaurants_user_password.value}'; GRANT CONNECT ON DATABASE restaurant_db TO restaurants_user; GRANT USAGE ON SCHEMA public TO restaurants_user; GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO restaurants_user;"
 }
 
 # creating the psql database
@@ -28,21 +30,9 @@ resource "azurerm_postgresql_flexible_server_database" "restaurant_db" {
   collation = "en_US.utf8"
 }
 
-resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_app" {
-  name             = "allow-app"
-  server_id        = azurerm_postgresql_flexible_server.db.id
-  start_ip_address = data.azurerm_container_app.restaurant_recommender.outbound_ip_addresses[0]
-  end_ip_address   = data.azurerm_container_app.restaurant_recommender.outbound_ip_addresses[0]
-}
-
-resource "random_password" "restaurants_user_password" {
-  length  = 20
-  special = true
-}
-
-resource "azurerm_postgresql_flexible_server_configuration" "restaurants_user" {
-  name      = "restaurants_user"
-  server_id = azurerm_postgresql_flexible_server.db.id
-  value     = "CREATE USER restaurants_user WITH PASSWORD '${random_password.restaurants_user_password.result}'; GRANT CONNECT ON DATABASE restaurant_db TO restaurants_user; GRANT USAGE ON SCHEMA public TO restaurants_user; GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO restaurants_user;"
-}
-
+# resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_app" {
+#   name             = "allow-app"
+#   server_id        = azurerm_postgresql_flexible_server.db.id
+#   start_ip_address = data.azurerm_container_app.restaurant_recommender.outbound_ip_addresses[0]
+#   end_ip_address   = data.azurerm_container_app.restaurant_recommender.outbound_ip_addresses[0]
+# }
