a4c0dccb2238e139e7dd986c617df07ceb60d9e8
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-13 10:22:47 +0200

    update kv name to meet global requirements

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "kv" {
-  name                = "restaurants-kv"
+  name                = "varonis-restaurants-kv"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
