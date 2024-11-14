72b800c792ddde649fdd909f4e112ce5c9df8d2e
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-11-14 10:37:55 +0200

    update

--- terraform/container-app.tf
+++ terraform/container-app.tf
@@ -1,12 +1,21 @@
 resource "azurerm_container_app_environment" "env" {
-  name                     = "restaurant-recommender-env"
-  location                 = data.azurerm_resource_group.rg.location
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  infrastructure_subnet_id = azurerm_subnet.priv_subnet.id
+  name                       = "restaurant-recommender-env"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  infrastructure_subnet_id   = azurerm_subnet.priv_subnet.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
 }
 
 resource "random_id" "random_id" {
   byte_length = 4
+}
+
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = "restaurant-recommender-logs"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_storage_account" "restaurants" {
